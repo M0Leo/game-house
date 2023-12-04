56181077d262104b89478f51ae61a366af7b5701
Mohamed Khaled <mohamedleo404@gmail.com>
2023-12-05 00:52:16 +0200

    add genres service func

--- src/genres/genres.service.ts
+++ src/genres/genres.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Genre } from 'src/entities/genres';
+import { In, Repository } from 'typeorm';
 
 @Injectable()
-export class GenresService {}
+export class GenresService {
+  constructor(
+    @InjectRepository(Genre)
+    private readonly genresRepository: Repository<Genre>,
+  ) {}
+
+  async getGenres() {
+    const genres = await this.genresRepository.find();
+    return genres;
+  }
+
+  async getGenre(id: number) {
+    const genre = await this.genresRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    return genre;
+  }
+
+  async createGenre(name: string) {
+    const genre = await this.genresRepository.save({ name });
+    return genre;
+  }
+
+  async updateGenre(id: number, name: string) {
+    const genre = await this.genresRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    genre.name = name;
+    await this.genresRepository.save(genre);
+    return genre;
+  }
+
+  async deleteGenre(id: number) {
+    const genre = await this.genresRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    await this.genresRepository.remove(genre);
+    return genre;
+  }
+
+  async getGenreByName(name: string) {
+    const genre = await this.genresRepository.findOne({
+      where: {
+        name,
+      },
+    });
+    return genre;
+  }
+
+  async getGenresByIds(ids: number[]) {
+    const genres = await this.genresRepository.findBy({ id: In(ids) });
+    return genres;
+  }
+}
